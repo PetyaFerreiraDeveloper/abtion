cd1e323e906057230111fe80351bca4c410037eb
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2023-02-14 12:03:27 +0100

    add days in forecast

--- components/DayForecast.js
+++ components/DayForecast.js
@@ -1,21 +1,70 @@
 import React from "react";
 import Image from "next/image";
 
-export const DayForecast = ({day}) => {
-    console.log(day);
-  return (
+export const DayForecast = ({ day, index }) => {
+  const icon = day.weather[0].icon;
+  const max = Math.round(day.main.temp_max);
+  const min = Math.round(day.main.temp_min);
+  const date = new Date(day.dt_txt);
+  const options = { weekday: "long" };
+  const dayOfWeek = new Intl.DateTimeFormat("en-US", options).format(date);
+  console.log(dayOfWeek);
+  console.log(day);
+  return index == 0 ? (
     <li className="flex items-center pl-8 pr-10 py-8 bg-[#F5C023] dark:bg-gray-400">
       <div className="pr-5">
         <Image
-          src={`http://openweathermap.org/img/w/04d.png`}
+          src={`http://openweathermap.org/img/w/${icon}.png`}
           width="80"
           height="80"
           alt=""
         />
       </div>
       <div className="flex flex-1 justify-between">
-        <p className="pr-">Tomorrow</p>
-        <p>max &deg;/ min &deg;</p>
+        <p className="pr-">Today</p>
+        <p>
+          <span>{max} &deg; </span>
+          <span>/</span>
+          <span> {min} &deg;</span>
+        </p>
+      </div>
+    </li>
+  ) : index % 2 == 0 ? (
+    <li className="flex items-center pl-8 pr-10 py-8 bg-[#F5C023] dark:bg-gray-400">
+      <div className="pr-5">
+        <Image
+          src={`http://openweathermap.org/img/w/${icon}.png`}
+          width="80"
+          height="80"
+          alt=""
+        />
+      </div>
+      <div className="flex flex-1 justify-between">
+        <p className="pr-">{dayOfWeek}</p>
+        <p>
+          <span>{max} &deg; </span>
+          <span>/</span>
+          <span> {min} &deg;</span>
+        </p>
+      </div>
+    </li>
+  ) : (
+    <li className="flex items-center pl-8 pr-10 py-8 bg-[#F5C023] dark:bg-gray-700">
+      <div className="pr-5">
+        <Image
+          src={`http://openweathermap.org/img/w/${icon}.png`}
+          width="80"
+          height="80"
+          alt=""
+        />
+      </div>
+      <div className="flex flex-1 justify-between">
+        <p className="pr-">{dayOfWeek}</p>
+        <p>
+          <span>{max} &deg; </span>
+          <span>/</span>
+          <span> {min} &deg;</span>
+        </p>
       </div>
     </li>
   );
--- components/NextWeek.js
+++ components/NextWeek.js
@@ -7,8 +7,10 @@ export const NextWeek = ({ forecast }) => {
     <div className="dark:text-white">
       <h2 className="text-[50px] font-extrabold px-10">This might be it</h2>
       <ul>
-        {forecast.list.map((day) => (
-          <DayForecast day={day} />
+        {forecast.list
+          .filter(hr => hr.dt_txt[12] === '2')
+          .map((day, index) => (
+          <DayForecast key={day.dt} day={day} index={index} />
         ))}
       </ul>
     </div>
