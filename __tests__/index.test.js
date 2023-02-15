a29acedbea2e982dbd4db6066960a3eef5390014
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2023-02-15 10:05:18 +0100

    start testing

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,16 +1,24 @@
 import React from "react";
 import Home from "../pages/index";
-import { findByRole, render, screen, within } from "@testing-library/react";
+import { Header } from "../components/Header";
+import { findByRole, render, screen, within, waitForElementToBeRemoved } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import userEvent from "@testing-library/user-event";
 import { tempData } from "../__mocks__/tempData";
 
+
 describe("Home Page", () => {
- it('shows heading temperature at curr location', () => {
-    render(<Home />);
-    const temp = screen.getByRole('heading', {name: /temperature/i});
-    expect(temp).toBeInTheDocument();
+ it('shows loading until loading data', () => {
+   render(<Home />);
+   const location = screen.getByText(/loading/i);
+   expect(location).toBeInTheDocument();
  });
+
+ it('renders a Header component, when the data is loaded', async () => {
+   const { getByText } = render(<Header />);
+
+   await waitFor(() => expect(getByText(/hvidovre/i)).toBeInTheDocument())
+ })
 
 
 
